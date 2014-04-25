9bebf3dd7f03ec5d875e28561a976217f1ea46ec
Max <max@motovilov.com>
2014-04-25 16:28:59 -0500

    Optimized detectTarget() for ipl scripts

--- include/targetFromBrowser.js
+++ include/targetFromBrowser.js
@@ -7,9 +7,17 @@
 @ once @
 
 	@@@ if( !target ) { @@@
-		var	requested$Targets = @= requested$Targets @=;
-		@= detectTarget @
+		var	requested$Targets = @= requested$Targets.values @;
 		@= make$Target @
+
+		function detectTarget(str) {
+			var m = @= new RegExp( "\\b(?:" + requested$Targets.contexts.join( "|" ) + ")\\b", "i" ) @.exec( str );
+			if( m ) 
+				for( var i=1; i<m.length; ++i )
+					if( m[i] )
+						return make$Target( requested$Targets[i-1], m[i] );
+		}
+
 	@@@ } @@@
 
 	var target =
